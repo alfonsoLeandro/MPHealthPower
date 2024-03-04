7343849337442599bffaa8d2536e9b6f5b18f40d
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-03-04 00:55:28 -0300

    Adds a check for every player's HP after clearing all.

--- src/main/java/com/github/alfonsoleandro/healthpower/commands/COR/ClearAllHandler.java
+++ src/main/java/com/github/alfonsoleandro/healthpower/commands/COR/ClearAllHandler.java
@@ -1,14 +1,20 @@
 package com.github.alfonsoleandro.healthpower.commands.COR;
 
 import com.github.alfonsoleandro.healthpower.HealthPower;
+import com.github.alfonsoleandro.healthpower.managers.health.AbstractHPManager;
 import com.github.alfonsoleandro.healthpower.utils.Message;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 public class ClearAllHandler extends AbstractHandler {
 
+    private final AbstractHPManager hpManager;
+
     public ClearAllHandler(HealthPower plugin, AbstractHandler successor) {
         super(plugin, successor);
+        this.hpManager = plugin.getHpManager();
     }
 
     @Override
@@ -24,8 +30,13 @@ public class ClearAllHandler extends AbstractHandler {
         }
 
         FileConfiguration hp = this.plugin.getHpYaml().getAccess();
-        hp.set("HP.players", null);
+        hp.set("HP.players", new String[0]);
         this.plugin.getHpYaml().save(true);
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            this.hpManager.checkAndCorrectHP(player);
+            this.messageSender.send(player, Message.YOUR_HP_CLEARED);
+        }
 
         this.messageSender.send(sender, Message.PLAYERS_CLEARED);
     }
