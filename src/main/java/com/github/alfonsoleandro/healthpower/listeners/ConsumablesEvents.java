e88e85a3d5c0ccad0b1aae1043ffe37dcdbc7619
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-03-04 01:08:09 -0300

    Reimplemented ConsumablesEvents.

--- src/main/java/com/github/alfonsoleandro/healthpower/listeners/ConsumablesEvents.java
+++ src/main/java/com/github/alfonsoleandro/healthpower/listeners/ConsumablesEvents.java
@@ -1,81 +1,74 @@
 package com.github.alfonsoleandro.healthpower.listeners;
 
 import com.github.alfonsoleandro.healthpower.HealthPower;
+import com.github.alfonsoleandro.healthpower.managers.ConsumableManager;
 import com.github.alfonsoleandro.healthpower.managers.health.AbstractHPManager;
+import com.github.alfonsoleandro.healthpower.utils.Consumable;
 import com.github.alfonsoleandro.healthpower.utils.Message;
+import com.github.alfonsoleandro.healthpower.utils.Settings;
 import com.github.alfonsoleandro.mputils.message.MessageSender;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.Objects;
 
 public class ConsumablesEvents implements Listener {
 
-    private final HealthPower plugin;
-    private final MessageSender<Message> messageSender;
     private final AbstractHPManager hpManager;
+    private final ConsumableManager consumableManager;
+    private final MessageSender<Message> messageSender;
+    private final Settings settings;
 
-    public ConsumablesEvents(HealthPower plugin){
-        this.plugin = plugin;
-        this.messageSender = plugin.getMessageSender();
+    public ConsumablesEvents(HealthPower plugin) {
+        this.consumableManager = plugin.getConsumableManager();
         this.hpManager = plugin.getHpManager();
+        this.messageSender = plugin.getMessageSender();
+        this.settings = plugin.getSettings();
     }
 
 
-    @EventHandler
-    public void onConsume(PlayerItemConsumeEvent event){
-        ConfigurationSection consumables = this.plugin.getConsumablesYaml().getAccess().getConfigurationSection("consumables");
-
-        if(consumables == null) return;
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onConsume(PlayerItemConsumeEvent event) {
         ItemStack consumed = event.getItem();
-
-        for(String consumable : consumables.getKeys(false)){
-            if(consumed.isSimilar(consumables.getItemStack(consumable+".item"))){
-                Player player = event.getPlayer();
-
-                if(!this.plugin.getConfig().getBoolean("config.consumables enabled")){
-                    this.messageSender.send(player, Message.CONSUMABLES_DISABLED);
-                    event.setCancelled(true);
-                    return;
-                }
-
-                if(consumables.contains(consumable+".options.add")){
-                    if(!this.hpManager.consumableAddHP(player, consumables.getDouble(consumable+".options.add"))){
-                        return;
-                    }
-
-                }else if(consumables.contains(consumable+".options.set")){
-                    if(!this.hpManager.consumableSetHP(player, consumables.getDouble(consumable+".options.set"))){
-                        return;
-                    }
-
-                }else{
-                    return;
-                }
-
-
-                String message;
-
-                if(consumables.contains(consumable+".options.message")){
-                    message = consumables.getString(consumable+".options.message")+"";
-                }else{
-                    message = this.messageSender.getString(Message.DEFAULT_CONSUMABLE_MSG);
-                }
-
-                this.messageSender.send(player, message.replace("%HP%", String.valueOf(this.hpManager.getHealth(player))));
-
-
-
-
+        if (consumed.hasItemMeta()) {
+            PersistentDataContainer persistentDataContainer = Objects.requireNonNull(consumed.getItemMeta()).getPersistentDataContainer();
+            if (!persistentDataContainer.has(this.consumableManager.getNamespacedKey())) {
                 return;
             }
+
+            if (!this.settings.isConsumablesEnabled()) {
+                this.messageSender.send(event.getPlayer(), Message.CONSUMABLES_DISABLED);
+                event.setCancelled(true);
+                return;
+            }
+
+            String consumableName = persistentDataContainer.get(this.consumableManager.getNamespacedKey(), PersistentDataType.STRING);
+            Consumable consumable = this.consumableManager.getConsumable(consumableName);
+
+            if (consumable == null) {
+                return;
+            }
+
+            Player player = event.getPlayer();
+
+            if (consumable.mode() == Consumable.ConsumableMode.ADD) {
+                this.hpManager.consumableAddHP(player, consumable.amount());
+            } else if (consumable.mode() == Consumable.ConsumableMode.SET) {
+                this.hpManager.consumableSetHP(player, consumable.amount());
+            }
+
+            String message = consumable.message() == null ? this.messageSender.getString(Message.DEFAULT_CONSUMABLE_MSG) : consumable.message();
+
+            this.messageSender.send(player, message.replace("%HP%", String.valueOf(this.hpManager.getHealth(player))));
         }
 
     }
-
-
 
 
 }
