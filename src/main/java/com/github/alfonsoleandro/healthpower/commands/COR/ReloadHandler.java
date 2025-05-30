4a7fbdaed746f2e7fe272211d337b89e096748ff
Leandro Alfonso <leamanya24.6@gmail.com>
2025-05-30 15:09:45 +0200

    Checks if any group's HP is under the minimum on reload.

--- src/main/java/com/github/alfonsoleandro/healthpower/commands/COR/ReloadHandler.java
+++ src/main/java/com/github/alfonsoleandro/healthpower/commands/COR/ReloadHandler.java
@@ -3,18 +3,24 @@ package com.github.alfonsoleandro.healthpower.commands.COR;
 import com.github.alfonsoleandro.healthpower.HealthPower;
 import com.github.alfonsoleandro.healthpower.managers.health.HPManager;
 import com.github.alfonsoleandro.healthpower.utils.Message;
+import com.github.alfonsoleandro.healthpower.utils.Settings;
+import com.github.alfonsoleandro.mputils.guis.utils.PlayersOnGUIsManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-//TODO
-public class ReloadHandler extends AbstractHandler{
+import java.util.Objects;
+
+public class ReloadHandler extends AbstractHandler {
 
     private final HPManager hpManager;
+    private final Settings settings;
 
     public ReloadHandler(HealthPower plugin, AbstractHandler successor) {
         super(plugin, successor);
         this.hpManager = plugin.getHpManager();
+        this.settings = plugin.getSettings();
     }
 
     @Override
@@ -24,20 +30,45 @@ public class ReloadHandler extends AbstractHandler{
 
     @Override
     protected void internalHandle(CommandSender sender, String label, String[] args) {
-        if(!sender.hasPermission("HealthPower.reload")) {
+        if (!sender.hasPermission("HealthPower.reload")) {
             this.messageSender.send(sender, Message.NO_PERMISSION);
             return;
         }
         this.plugin.reload(false);
 
-        if(this.plugin.getConfig().getBoolean("config.check HP on reload")){
-            for (Player p : Bukkit.getOnlinePlayers()){
+        if (this.plugin.getConfig().getBoolean("config.check HP on reload")) {
+            for (Player p : Bukkit.getOnlinePlayers()) {
                 this.hpManager.checkAndCorrectHP(p);
             }
             this.messageSender.send(sender, Message.PLAYERS_CHECKED);
         }
 
-//        this.playersOnGUIsManager.removeAll(); GUI close? //TODO
+        // Closes the GUI for all players in case anyone was buying HP
+        PlayersOnGUIsManager.removeAll("MPHealthPower:SHOP");
+
+        FileConfiguration hpFile = this.plugin.getHpYaml().getAccess();
+
+        if (hpFile.contains("HP.groups") && this.settings.isUseGroupsSystem()) {
+            double minimumHP = this.settings.getMinimumHP();
+            for (String groupName : Objects.requireNonNull(hpFile.getConfigurationSection("HP.groups"))
+                    .getKeys(false)) {
+                double groupHp = hpFile.getDouble("HP.groups." + groupName);
+                if (groupHp < minimumHP) {
+                    if(!sender.equals(Bukkit.getConsoleSender())){
+                        this.messageSender.send(sender, Message.GROUP_HP_UNDER_MINIMUM,
+                                "%group%", groupName,
+                                "%HP%", String.valueOf(groupHp),
+                                "%minimum%", String.valueOf(minimumHP));
+                    }
+                    this.messageSender.send(Bukkit.getConsoleSender(), Message.GROUP_HP_UNDER_MINIMUM,
+                            "%group%", groupName,
+                            "%HP%", String.valueOf(groupHp),
+                            "%minimum%", String.valueOf(minimumHP));
+                }
+
+            }
+        }
+
         this.messageSender.send(sender, "&aFiles reloaded");
     }
 }
