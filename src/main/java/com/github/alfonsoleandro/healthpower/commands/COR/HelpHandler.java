3ad09fb28893d863746c45d30108427b50468770
Leandro Alfonso <leamanya24.6@gmail.com>
2025-09-23 12:11:10 +0200

    Put help commands in alphabetic order

--- src/main/java/com/github/alfonsoleandro/healthpower/commands/COR/HelpHandler.java
+++ src/main/java/com/github/alfonsoleandro/healthpower/commands/COR/HelpHandler.java
@@ -18,18 +18,18 @@ public class HelpHandler extends AbstractHandler{
     protected void internalHandle(CommandSender sender, String label, String[] args) {
         this.messageSender.send(sender, "&6List of commands");
         this.messageSender.send(sender, "&f/"+label+" help");
-        this.messageSender.send(sender, "&f/"+label+" version");
-        this.messageSender.send(sender, "&f/"+label+" reload");
-        this.messageSender.send(sender, "&f/"+label+" set (player) (HP)");
         this.messageSender.send(sender, "&f/"+label+" add (player) (HP)");
-        this.messageSender.send(sender, "&f/"+label+" gui");
-        this.messageSender.send(sender, "&f/"+label+" group set (group) (HP)");
+        this.messageSender.send(sender, "&f/"+label+" checkAll");
+        this.messageSender.send(sender, "&f/"+label+" check (name)");
+        this.messageSender.send(sender, "&f/"+label+" clearAll");
+        this.messageSender.send(sender, "&f/"+label+" clear (name)");
         this.messageSender.send(sender, "&f/"+label+" consumable give (player) (name)");
         this.messageSender.send(sender, "&f/"+label+" consumable set (name) (add/set) (amount)");
-        this.messageSender.send(sender, "&f/"+label+" clear (name)");
-        this.messageSender.send(sender, "&f/"+label+" clearAll");
-        this.messageSender.send(sender, "&f/"+label+" check (name)");
-        this.messageSender.send(sender, "&f/"+label+" checkAll");
+        this.messageSender.send(sender, "&f/"+label+" group set (group) (HP)");
+        this.messageSender.send(sender, "&f/"+label+" gui");
+        this.messageSender.send(sender, "&f/"+label+" reload");
+        this.messageSender.send(sender, "&f/"+label+" set (player) (HP)");
+        this.messageSender.send(sender, "&f/"+label+" version");
 
     }
 }
