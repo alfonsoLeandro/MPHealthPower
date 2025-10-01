cdbf2255c217e345808f2151a723af4078daeca5
Leandro Alfonso <leamanya24.6@gmail.com>
2025-10-01 20:21:52 +0200

    Adds FormulaGUIData.

---
+++ src/main/java/com/github/alfonsoleandro/healthpower/managers/health/formula/gui/FormulaGUIData.java
@@ -0,0 +1,4 @@
+package com.github.alfonsoleandro.healthpower.managers.health.formula.gui;
+
+public record FormulaGUIData(FormulaGUIMenu guiMenu, String worldName, String formulaRawString, Integer order) {
+}
---
+++ src/main/java/com/github/alfonsoleandro/healthpower/managers/health/formula/gui/FormulaGUIMenu.java
@@ -0,0 +1,7 @@
+package com.github.alfonsoleandro.healthpower.managers.health.formula.gui;
+
+public enum FormulaGUIMenu {
+    FORMULAS,
+    FORMULAS_FOR_WORLD,
+    CREATE
+}
