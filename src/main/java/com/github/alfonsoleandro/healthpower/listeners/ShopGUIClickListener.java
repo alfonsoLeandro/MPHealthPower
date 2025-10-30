c2da18849703baef5dabea9f0227a346ede80735
Leandro Alfonso <leamanya24.6@gmail.com>
2025-10-30 21:34:14 +0100

    Implements Shop refactor.

--- src/main/java/com/github/alfonsoleandro/healthpower/listeners/ShopGUIClickListener.java
+++ src/main/java/com/github/alfonsoleandro/healthpower/listeners/ShopGUIClickListener.java
@@ -1,6 +1,8 @@
 package com.github.alfonsoleandro.healthpower.listeners;
 
 import com.github.alfonsoleandro.healthpower.HealthPower;
+import com.github.alfonsoleandro.healthpower.managers.health.formula.FormulaManager;
+import com.github.alfonsoleandro.healthpower.managers.health.formula.PlayerHpData;
 import com.github.alfonsoleandro.healthpower.managers.shop.HPShopItem;
 import com.github.alfonsoleandro.healthpower.managers.shop.HPShopManager;
 import com.github.alfonsoleandro.healthpower.managers.health.HPManager;
@@ -17,13 +19,17 @@ public class ShopGUIClickListener implements Listener {
 
     private final MessageSender<Message> messageSender;
     private final HPManager hpManager;
-    private final HPShopManager hpGUIManager;
+    private final HPShopManager hpShopManager;
+    private final FormulaManager formulaManager;
+    private final Settings settings;
     private final Economy economy;
 
     public ShopGUIClickListener(HealthPower plugin) {
         this.messageSender = plugin.getMessageSender();
         this.hpManager = plugin.getHpManager();
-        this.hpGUIManager = plugin.getHpGUIManager();
+        this.hpShopManager = plugin.getHpGUIManager();
+        this.formulaManager = plugin.getFormulaManager();
+        this.settings = plugin.getSettings();
         this.economy = plugin.getEconomy();
     }
 
@@ -34,13 +40,13 @@ public class ShopGUIClickListener implements Listener {
             return;
         }
         int slot = event.getRawSlot();
-        if (slot >= event.getGui().getSize()) {
+        if (slot >= event.getGui().getSize() || slot < 0) {
             return;
         }
         event.setCancelled(true);
 
         // get gui item from raw slot, from hp manager
-        HPShopItem item = this.hpGUIManager.getItem(slot);
+        HPShopItem item = this.hpShopManager.getItem(slot);
 
         // get type from gui item
         // if type is info, return
@@ -50,10 +56,19 @@ public class ShopGUIClickListener implements Listener {
 
         Player player = (Player) event.getWhoClicked();
 
+        if (!this.hpShopManager.hasPermissionForItem(player, slot)) {
+            this.messageSender.send(player, Message.SHOP_ITEM_NO_PERMISSION);
+            return;
+        }
+
+        if (!this.hpShopManager.meetsRequirementForItem(player, slot)) {
+            this.messageSender.send(player, Message.SHOP_ITEM_REQUIREMENTS_NOT_MET);
+            return;
+        }
+
         // get price from gui item
         double balance = this.economy.getBalance(player);
-        double currentHP = this.hpManager.getHealth(player);
-        double price = this.hpGUIManager.getPrice(slot, currentHP);
+        double price = this.hpShopManager.getPriceForItem(player, slot);
 
         // check if player has enough money
         if (price > 0 && price > balance) {
@@ -64,43 +79,88 @@ public class ShopGUIClickListener implements Listener {
             return;
         }
 
-        // validate amount
-        if (item.amount() <= 0) {
-            this.messageSender.send(player, Message.CANNOT_SET_HP_UNDER_0);
-            player.closeInventory();
-            return;
+        if (this.settings.isCheckLimitsForShop()) {
+            // Simulate to check if transaction is possible
+            String worldName = player.getWorld().getName();
+            Double hpCap = this.hpManager.getHpCapForWorld(worldName);
+            Double hpFloor = this.hpManager.getHpFloorForWorld(worldName);
+            PlayerHpData playerHpData = this.formulaManager.getPlayerHpData(player);
+            double currentVal = playerHpData.shopHp() == null ? 0 : playerHpData.shopHp();
+            double amount = item.amount();
+            boolean hasCap = hpCap != null && hpCap != -1;
+
+            PlayerHpData simulated;
+            switch (item.type()) {
+                case ADD:
+                    if (!hasCap) break;
+                    simulated = new PlayerHpData(playerHpData.baseHp(), playerHpData.groupHp(), playerHpData.permissionHp(), currentVal + amount);
+                    double newValue = this.formulaManager.simulate(player, worldName, simulated);
+                    if (newValue > hpCap) {
+                        this.messageSender.send(player, Message.YOUR_HP_ABOVE_CAP);
+                        return;
+                    }
+                    break;
+                case SET:
+                    simulated = new PlayerHpData(playerHpData.baseHp(), playerHpData.groupHp(), playerHpData.permissionHp(), amount);
+                    double newSetValue = this.formulaManager.simulate(player, worldName, simulated);
+                    if ((hasCap && newSetValue > hpCap)) {
+                        this.messageSender.send(player, Message.YOUR_HP_ABOVE_CAP);
+                        return;
+                    }
+                    if (newSetValue < hpFloor) {
+                        this.messageSender.send(player, Message.YOUR_HP_BELOW_FLOOR);
+                        return;
+                    }
+                    break;
+                case REMOVE:
+                    simulated = new PlayerHpData(playerHpData.baseHp(), playerHpData.groupHp(), playerHpData.permissionHp(), currentVal - amount);
+                    double newRemoveValue = this.formulaManager.simulate(player, worldName, simulated);
+                    if (newRemoveValue < hpFloor) {
+                        this.messageSender.send(player, Message.NOT_ENOUGH_HP,
+                                "%hp%", String.valueOf(item.amount()));
+                        player.closeInventory();
+                        return;
+                    }
+            }
+        }
+
+        if (item.message() != null) {
+            this.messageSender.send(player, item.message()
+                    .replace("%amount%", String.valueOf(item.amount())));
         }
 
         // finally, add/set/remove hp
         switch (item.type()) {
             case ADD:
-                if (this.hpManager.cannotAddHP(player, item.amount())) {
-                    this.messageSender.send(player, Message.YOUR_HP_ABOVE_CAP);
-                    return;
-                }
                 chargeOrPay(price, player);
-                this.hpManager.consumableOrGUIAddHP(player, item.amount());
+                this.hpManager.addShopHP(player, item.amount());
+                if (item.message() == null) {
+                    this.messageSender.send(player, Message.SHOP_ADD_HP,
+                            "%amount%", String.valueOf(item.amount()));
+                }
+                break;
             case SET:
-                if (this.hpManager.cannotSetHP(player, item.amount())) {
-                    this.messageSender.send(player, Message.YOUR_HP_ABOVE_CAP);
-                    return;
-                }
                 chargeOrPay(price, player);
-                this.hpManager.consumableOrGUISetHP(player, item.amount());
+                this.hpManager.setShopHP(player, item.amount());
+                if (item.message() == null) {
+                    this.messageSender.send(player, Message.SHOP_SET_HP,
+                            "%amount%", String.valueOf(item.amount()));
+                }
+                break;
             case REMOVE:
-                if (this.hpManager.getHealth(player) < item.amount()) {
-                    this.messageSender.send(player, Message.NOT_ENOUGH_HP,
-                            "%hp%", String.valueOf(item.amount()));
-                    player.closeInventory();
-                    return;
-                }
                 chargeOrPay(price, player);
-                this.hpManager.guiRemoveHP(player, item.amount());
+                this.hpManager.addShopHP(player, -item.amount());
+                if (item.message() == null) {
+                    this.messageSender.send(player, Message.SHOP_REMOVE_HP,
+                            "%amount%", String.valueOf(item.amount()));
+                }
+
         }
 
+        // Check and correct player's HP
+        this.hpManager.checkAndCorrectHP(player);
         // Re-open the GUI if everything went correctly, to update descriptions
-        this.hpGUIManager.openGUI(player);
-
+        this.hpShopManager.openGUI(player);
     }
 
     private void chargeOrPay(double price, Player player) {
